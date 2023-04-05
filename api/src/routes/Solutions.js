3335b7947640ccef79de8490227115687fd353d4
AgustinDi <agustindigiacinto18@gmail.com>
2023-04-05 09:59:47 -0300

    Solution 1 rewrite 1

--- api/src/routes/Solutions.js
+++ api/src/routes/Solutions.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 let router = Router();
 
 router.get('/1', async (req, res, next) => {
-    const random = Math.random() * 100;
+    const random = parseInt(Math.random() * 100);
     res.send({result: random == 0 ? 100 : random})
 })
 
