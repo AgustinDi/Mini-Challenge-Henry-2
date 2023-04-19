2c4b8025f2f2ad5411ceaccb77a2afb7f8570b3e
AgustinDi <agustindigiacinto18@gmail.com>
2023-04-19 12:00:53 -0300

    data rendered, activities redirect working for sqlActivities

--- client/src/components/activities.jsx
+++ client/src/components/activities.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {data} from '../helper/activity.json';
+import dataSql from '../helper/sqlActivity.json';
 const imgSQL = import.meta.env.VITE_IMG_SQL;
 const url = import.meta.env.VITE_URL;
 
@@ -9,8 +10,11 @@ export default function Activities() {
             <ol>
               {data.map((activity,index)=><li key={index}>{activity}: <a href={url + 'solution/' + (index+1)} target="_blank">Resultado</a></li>)}
               <li>Dada la siguiente base de datos relacional: <br/>
-                <img src={imgSQL}></img>
-                
+                <img src={imgSQL}></img> <br/>
+                Resolver las Siguientes consultas en sql:
+                <ol>
+                    {dataSql.data.map((act,index)=><li key={index}>{act}: <a href={url + 'solution/5/' + (index+1)} target="_blank">Resultado</a></li>)}
+                </ol>
               </li>
             </ol>
         </div>
